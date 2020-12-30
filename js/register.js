8a23cd77262cceaa0fa981a7aca17cfce09022b1
wftech <54228563+techwf@users.noreply.github.com>
2020-12-30 14:09:47 +0800

    Create register.js

---
+++ js/register.js
@@ -0,0 +1,43 @@
+
+var dg1 = Date.now();
+var content = "/Content/Images/100KB.jpg?v=";
+var template = "";
+var tmp=null;
+var tmp_register=null;
+
+function is_mobile(){
+     var userAgentInfo = navigator.userAgent;
+     var Agents = new Array("Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod");  
+     var flag = false;  
+     for (var v = 0; v < Agents.length; v++) {  
+         if (userAgentInfo.indexOf(Agents[v]) > 0) { flag = true; break; }  
+     }  
+     return flag;
+}
+
+function auto(self){
+    if(tmp===null){
+        tmp_register=$(self).attr("data");
+        console.log(tmp_register);
+        tmp=tmp_register;
+        tmp += "/agentset?domain=" + seller;
+    }
+}
+
+function close_window(win, tmp){
+    setTimeout(function () { win.close(); window.location.href = tmp_register+'/Mobile/Register/Register'; }, 2000);
+}
+
+$("document").ready(function(){
+    $.ajax({
+        url: "/popups/get-urls.php?seller=" + seller,
+        async: false,
+        success: function( data ) {
+            for(var i=0;i<data.length;i++)
+            {
+                template += "<img src=" + data[i] + content + dg1 + " data='"+data[i]+"' width=30 height=30 onload='auto(this);'>";
+            }
+            $("#mb_ad").html(template);
+        }
+    });
+})
